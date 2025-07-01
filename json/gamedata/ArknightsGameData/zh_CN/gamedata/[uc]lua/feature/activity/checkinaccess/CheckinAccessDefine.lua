5773758c7d5436445436c3d2614b2669cc37e136
PuppiizSunniiz <114895835+PuppiizSunniiz@users.noreply.github.com>
2025-07-01 22:23:04 +0700

    Submodule Test

--- json/gamedata/ArknightsGameData/zh_CN/gamedata/[uc]lua/feature/activity/checkinaccess/CheckinAccessDefine.lua
+++
@@ -1,9 +0,0 @@
-CheckinAccessStatus = {
-  CAN_CONFIRM = 0,
-  CONFIRMED = 1,
-  ALL_CONFIRMED = 2,
-}
-
-CheckinAccessServiceCode = {
-  GET_REWARD = "/activity/actCheckinAccess/getCheckInReward",
-}
