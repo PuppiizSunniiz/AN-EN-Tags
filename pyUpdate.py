504568afa1599fc1ea71872bb856a9cf58c594bd
PuppiizSunniiz <114895835+PuppiizSunniiz@users.noreply.github.com>
2026-01-16 15:25:40 +0700

    [CN] Medjehtiqedti Bound (EX Stage)

--- pyUpdate.py
+++ pyUpdate.py
@@ -72,7 +72,7 @@ json_talentTL       =   json_load("json/ace/tl-talents.json")
 # New
 #########################################################################################################
 #["OpsName#1","OpsName#2", ...]
-NEW_CHARS : list[str] = ["Titi","Perfumer the Distilled","Varkáris",] # "", 
+NEW_CHARS : list[str] = [] # "", 
 
 #["ItemID#1","ItemID#2", ...]
 NEW_MATS : list[str] = [] # "",
